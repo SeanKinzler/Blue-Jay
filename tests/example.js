2bfa6ec10bbe0a96244690ebc0e1f2c75adb8de5
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-22 20:26:00 -0700

    Change test.js to example.js

--- tests/example.js
+++ tests/example.js
@@ -16,7 +16,7 @@ describe('jsDOM', function () {
   it('should grab a window, given a url', function () {
     jsdom.env({
       url: 'http://www.google.com',
-      scripts: ["http://code.jquery.com/jquery.js"],
+      scripts: ['http://code.jquery.com/jquery.js'],
       done: function (errors, window) {
         expect(!!errors).to.equal(false);
         expect(!!window).to.equal(true);
