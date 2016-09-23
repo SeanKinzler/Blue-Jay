98ef6232cbcf4a73dc36cca9a2069ec98b89b1b9
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-22 20:23:09 -0700

    Add basic jsdom tests

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,6 @@
 var chai = require('chai');
 var sinon = require('sinon');
+var jsdom = require('jsdom');
 
 var should = chai.should;
 var expect = chai.expect;
@@ -9,4 +10,19 @@ describe('Example tests', function () {
   it('should pass for truthy values', function () {
     expect(true).to.equal(!!1);
   });
+});
+
+describe('jsDOM', function () {
+
+  it('should grab a window, given a url', function () {
+    jsdom.env({
+      url: 'http://www.google.com',
+      done: function (errors, window) {
+        expect(!!errors).to.equal(false);
+        expect(!!window).to.equal(true);
+      }
+    });
+  });
+
+  
 });
