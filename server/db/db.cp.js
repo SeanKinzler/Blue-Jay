7c943fabc54b34d8728963cd7fa6828c3e3fc9ad
seankinzler <sean.kinzler92@gmail.com>
2016-09-24 15:35:29 -0700

    prepped for deployed db

--- server/db/db.cp.js
+++ server/db/db.cp.js
@@ -1,7 +1,8 @@
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('test', 'root', 'root', {
-  host: 'localhost',
+var db = new Sequelize('bluejay', /*username*/'', /*pass*/'', {
+  host:'',//str, does not include port
+  port:,//index
   dialect: 'mysql'
 });
 module.exports = db;
