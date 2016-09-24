b4721e1d4b6cb7b2d8b894d3bc769d6a2fe0aaf7
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-24 12:14:56 -0700

    Configure server to use https

--- server/config/app-config.js
+++ server/config/app-config.js
@@ -2,8 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser').json;
 var path = require('path');
 
+
 var app = express();
 app.use(bodyParser());
+
 app.use(express.static(path.join(__dirname, '../../client')));
 
 app.post('/users/login', function (req, res) {
@@ -13,5 +15,11 @@ app.post('/users/login', function (req, res) {
 app.post('/users/signup', function (req, res) {
   res.send({'data': 'Signed up!'});
 });
+
+
+
+
+
+
 
 module.exports = app;
