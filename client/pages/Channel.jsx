e997f72fdbb8d80cddc29ea1c72d8319ccbdccb7
cassac <hicassac@gmail.com>
2016-09-27 16:38:14 -0700

    reduces header size

--- client/pages/Channel.jsx
+++ client/pages/Channel.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router';
 export default ({params}) => {
   return(
     <div className=''>
-      <h1 className='center-align'>Channel: { `${params.channelId}` }</h1>
+      <h4 className='center-align'>Channel: { `${params.channelId}` }</h4>
       <Video />
       <div id='infoPane'>
         <div className='teal' id='infoBanner'>
