8f238f70416ece389125453bf058d07e3c6178ec
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-27 16:26:08 -0700

    Delete all tutorial WebRTC code for refactor

--- server/index.js
+++ server/index.js
@@ -16,69 +16,15 @@ var fileServer = new nodeStatic.Server();
 var server = https.createServer(serverConfig, app);
 
 var io = socketIO.listen(server);
+
 io.sockets.on('connection', function(socket) {
-  console.log('connected!');
-  // convenience function to log server messages on the client
-  var log = function () {
-    var array = ['Message from server:'];
-    array.push.apply(array, arguments);
-    socket.emit('log', array);
-  };
+  var user;
 
-  socket.on('message', function(message) {
-    log('Client said: ', message);
-    // for a real app, would be room-only (not broadcast)
-    socket.broadcast.emit('message', message);
+  socket.on('start', function (data) {
+    user = data;
+    console.log(user);
   });
 
-  socket.on('create or join', function(room) {
-    // console.log(room);
-    log('Received request to create or join room ' + room);
-
-    if (socket.adapter.rooms[room] === undefined) {
-      socket.join(room);
-    } else if (socket.adapter.rooms[room].length < 2) {
-      socket.join(room);
-    } else {
-      socket.emit('full', room);
-    }
-
-
-    var numClients = socket.adapter.rooms[room].length;
-    console.log(socket.adapter.rooms);
-    
-    log('Room ' + room + ' now has ' + numClients + ' client(s)');
-
-    if (numClients === 1) {
-      socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
-      socket.emit('created', room, socket.id);
-
-    } else if (numClients === 2) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
-      socket.join(room);
-      socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready');
-    } else { // max two clients
-      
-    }
-  });
-
-  socket.on('ipaddr', function() {
-    var ifaces = os.networkInterfaces();
-    for (var dev in ifaces) {
-      ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-          socket.emit('ipaddr', details.address);
-        }
-      });
-    }
-  });
-
-  socket.on('bye', function() {
-    console.log('received bye');
-  });
 });
 
 
