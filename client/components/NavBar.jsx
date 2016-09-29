01fe8f2b62097ba8083e5b94b4d5eebe0100c191
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-29 12:15:56 -0700

    Change color of navbar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 export default () => {
 	return (
 		<nav>
-		  <div className='nav-wrapper blue'>
+		  <div className='nav-wrapper blue darken-1'>
         <Link className='brand-logo center' to='/'>BlueBird</Link>
   		</div>
 		</nav>
