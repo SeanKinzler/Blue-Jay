626aa37c91e9049e0169afa098bd2ee9fdfba82c
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-29 12:27:34 -0700

    change navbar stylingwq

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default () => {
-	return (
-		<nav>
-		  <div className='nav-wrapper blue darken-1'>
+  return (
+    <nav>
+      <div className='nav-wrapper blue darken-1'>
         <Link className='brand-logo center' to='/'>BlueBird</Link>
-  		</div>
-		</nav>
-	)
+      </div>
+    </nav>
+  );
 }
