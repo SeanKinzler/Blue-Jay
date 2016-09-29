c1337bde67eae0853b0980fe9d874fe6ed8d2d9b
cassac <hicassac@gmail.com>
2016-09-29 15:19:01 -0700

    adds user signout function and redirect

--- client/actions/index.jsx
+++ client/actions/index.jsx
@@ -1,6 +1,15 @@
+import { browserHistory } from 'react-router';
+
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const REQUEST_CHANNELS = 'REQUEST_CHANNELS';
 export const SIGN_IN_USER = 'SIGN_IN_USER';
 export const SIGN_OUT_USER = 'SIGN_OUT_USER';
 export const AUTH_ERROR = 'AUTH_ERROR';
+
+export const signOutUser = () => {
+	browserHistory.push('/');
+	return {
+		type: SIGN_OUT_USER
+	}
+}
