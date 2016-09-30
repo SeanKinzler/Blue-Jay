8b3ac34eacd98310f9c1b238f00389d34f23b3f7
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-30 12:23:16 -0700

    Change db connection config

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,8 @@
 var Sequelize = require('sequelize');
 
 var db = new Sequelize('bluejay', 'root', process.env.DBPASSWORD, {
-  host: process.env.DBHOST,
+  host: 'mysql:' + process.env.DBHOST,
   port: Number(process.env.DBPORT),
-  dialect: 'mysql'
 });
 
 module.exports = db;
