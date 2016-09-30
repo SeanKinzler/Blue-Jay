04355aed49026e2063d252eb95534743334a2d98
ZolayvarE <ZolayvarECU@gmail.com>
2016-09-30 14:21:12 -0700

    change default timeout for tests

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,5 @@
 var Sequelize = require('sequelize');
 
-process.env.DBHOST = 'blue-jay-users.cnt8aiilpqcs.us-west-2.rds.amazonaws.com';
-process.env.DBPORT = 3306;
-process.env.DBPASSWORD = 'lionHead2';
-
 var db = new Sequelize('bluejay', 'root', process.env.DBPASSWORD, {
   host: process.env.DBHOST,
   port: Number(process.env.DBPORT),
