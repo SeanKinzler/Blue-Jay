e7f7a2b1ea16e9c3e0a63fe88b51ac826fa98627
cassac <hicassac@gmail.com>
2016-09-30 15:42:35 -0700

    implements persistent state

--- client/store/storeConfig.jsx
+++ client/store/storeConfig.jsx
@@ -1,12 +1,14 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index.jsx';
+import { autoRehydrate } from 'redux-persist';
 
 const storeConfig = (initialState) => {
 	return createStore(
 		rootReducer,
 		initialState,
-		compose (
+		compose(
+			autoRehydrate(),
 			applyMiddleware(thunk),
 			window.devToolsExtension ? window.devToolsExtension() : f => f
 		)
