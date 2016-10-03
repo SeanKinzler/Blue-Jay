cb073542268f8013692bf69fa1f2028dfc6a44f7
cassac <hicassac@gmail.com>
2016-10-02 18:40:46 -0700

    extends search container

--- client/pages/Search.jsx
+++ client/pages/Search.jsx
@@ -2,20 +2,64 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as Actions from '../actions/index.jsx';
 import SearchBar from '../components/SearchBar.jsx';
+import SearchFilter from '../components/SearchFilter.jsx';
 
 class Search extends Component {
+
+	componentDidMount() {
+		$('select').material_select();
+		$('select').on(
+			'change', 
+			(e) => {
+				var optionsText = [];
+				var title = e.target.id;
+				var handler = e.target.dataset.handler;
+				var optionsElements = e.target.selectedOptions;
+				for (var i = 0; i < optionsElements.length; i++) {
+					optionsText.push(optionsElements[i].text);
+				}
+				this.props[handler](optionsText)
+			}
+		) 
+	}
 	render() {
 		return (
-			<div>
-				<h1>Search Page</h1>
+			<div className='container'>
 				<SearchBar onTermChange={this.props.searchChannels} />
+				<div className='row'>
+					<div className='col s2'>
+						<SearchFilter filterOptions={this.props.categories} />
+					</div>
+					<div className='col s2'>
+						<SearchFilter filterOptions={this.props.price} />
+					</div>
+					<div className='col s2'>
+						<SearchFilter filterOptions={this.props.type} />
+					</div>
+				</div>
 			</div>
 		)
 	}
 } 
 
 const mapStateToProps = (state) => {
-	return {}
+	return {
+		categories: {
+			title: 'Categories', 
+			handler: 'filterChannelCategories',
+			data: ['Math', 'Entertainment', 'History', 'Politics']
+		},
+		price: {
+			title: 'Price',
+			handler: 'filterChannelPrices',
+			data: ['Free', 'Paid']
+		},
+		type: {
+			title: 'Type',
+			handler: 'filterChannelType',
+			data: ['Live', 'Archived']
+		}		
+	}
 }
 
 const mapDispatchToProps = (dispatch) => {
