e7e1daecdfc64ab7c3728209eb024028db0b999a
cassac <hicassac@gmail.com>
2016-10-02 18:40:10 -0700

    styling and placeholder

--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const SearchBar = ({ onTermChange }) => {
 	return (
 		<input 
+			style={{'text-align': 'center'}}
 			type='text' 
+			placeholder='Search for a stream'
 			onChange={ e => onTermChange(e.target.value) } 
 		/>
 	)
