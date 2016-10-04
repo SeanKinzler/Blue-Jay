0b7c60aab59ff059f0a92574e96d19ae24373742
cassac <hicassac@gmail.com>
2016-10-04 16:46:39 -0700

    adds template for compact results component

--- client/components/SearchResultsCompact.jsx
+++ client/components/SearchResultsCompact.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const SearchResultsCompact = ({searchResults}) => {
 	return (
 		<div>
-			Compact search results here.
+			{ searchResults.map((s) => {
+				return (
+					<div key={s.id} className='col s4'>
+						<div className="card blue">
+							<div className="card-content white-text">
+								<span className="card-title">Card Title</span>
+								<p>I am a very simple card. I am good at containing small bits of information.
+									I am convenient because I require little markup to use effectively.</p>
+							</div>
+							<div className="card-action">
+								<a href="#">This is a link</a>
+								<a href="#">This is a link</a>
+							</div>
+						</div>
+					</div>
+				)
+			})}
 		</div>
 	)
 }
