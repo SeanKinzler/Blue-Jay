1bc2b7c6fd11508d1b73adc1dae1afe8de68b9f6
ZolayvarE <ZolayvarECU@gmail.com>
2016-10-03 17:26:06 -0700

    Refactor server file structure

---
+++ client/lib/treeRTC.js
@@ -0,0 +1,253 @@
+module.exports = function (room, user, socket) {
+  var parent = null;
+  var children = [null, null];
+
+  var STUN = {
+    urls: 'stun:stun.l.google.com:19302'
+  };
+
+  var TURN = {
+    urls: 'turn:turn.bistri.com:80',
+    credential: 'homeo',
+    username: 'homeo'
+  };
+
+  var ICE = {
+    iceServers: [STUN, TURN]
+  };
+
+  // FOR KINZLER TO EDIT. //////////////////////////////////////////////////////////////////////
+
+  var addVideo = function (source, elementId) {
+
+    var remoteVideo = document.getElementById('remoteVideo');
+
+    if (!remoteVideo.src) {
+
+      remoteVideo.src = window.URL.createObjectURL(source);
+
+    } else {
+
+      var newVideo = document.createElement('video');
+      newVideo.className = 'col s3';
+      newVideo.id = elementId;
+      newVideo.src = window.URL.createObjectURL(source);
+
+      document.getElementById('putVidsHere').appendChild(newVideo);
+    }
+  };
+
+  var removeVideo = function (id) {
+
+    var addedVideo = document.getElementById(id);
+
+    if (!addedVideo) {
+
+      document.getElementById('remoteVideo').src = '';
+
+    } else {
+
+      addedVideo.parentNode.removeChild(addedVideo);
+
+    }
+
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////
+
+  var askForCamera = function () {
+    navigator.getUserMedia({
+      video: true,
+      audio: false, 
+    }, function (stream) {
+      var localSrc = window.URL.createObjectURL(stream);
+      document.getElementById('localVideo').src = localSrc;
+    }, function (error) {
+      setTimeout(function () {
+        askForCamera();
+      }, 500);
+    });
+  };
+
+  var sendOffer = function (targetUserId, yourUserId) {
+    navigator.getUserMedia({
+      video: true,
+      audio: true
+    }, function (mediaStream) {
+      parent = new RTCPeerConnection(ICE);
+
+      parent.addStream(mediaStream);
+
+      parent.onaddstream = function (media) {
+        addVideo(media.stream, targetUserId);
+      };
+
+      parent.onicecandidate = function (event) {
+        if (!!event.candidate) {
+          socket.emit('ice-candidate', {
+            recipient: targetUserId,
+            candidate: event.candidate,
+            returnAddress: yourUserId,
+          });
+        } else {
+          socket.emit('ice-merge', {
+            recipient: targetUserId,
+            sdp: parent.localDescription,
+            returnAddress: yourUserId,
+          });
+        }
+      };
+
+      parent.oniceconnectionstatechange = function(event) {
+        if (peers[id].iceConnectionState === 'completed') {
+          socket.emit('ice-merge', {
+            recipient: targetUserId,
+            sdp: peers[id].localDescription,
+            returnAddress: yourUserId,
+          });
+        } else if (peers[id].iceConnectionState === 'disconnected') {
+          removeVideo(targetUserId);
+        }
+      };
+
+      parent.createOffer(function (offerObj) {
+        peers[id].setLocalDescription(offerObj);
+        socket.emit('offer', {
+          recipient: targetUserId,
+          offer: offerObj,
+          returnAddress: yourUserId,
+        });
+        console.log('Sent offer!');
+      }, function (error) {
+        console.log(error);
+      });
+    }, function (error) {
+      console.log(error);
+    });
+  };
+
+  var sendAnswer = function (receivedData) {
+    peers.length++;
+    var id = receivedData.returnAddress;
+    navigator.getUserMedia({
+      video: true,
+      audio: true,
+    }, function (mediaStream) {
+      peers[id] = new RTCPeerConnection(ICE);
+
+      peers[id].addStream(mediaStream);
+
+      peers[id].onaddstream = function (media) {
+        addVideo(media.stream, receivedData.returnAddress);
+      };
+
+      peers[id].onicecandidate = function (event) {
+        if (!!event.candidate) {
+          socket.emit('ice-candidate', {
+            recipient: receivedData.returnAddress,
+            candidate: event.candidate,
+            returnAddress: receivedData.recipient,
+          });
+        } else {
+          socket.emit('ice-merge', {
+            recipient: receivedData.returnAddress,
+            sdp: peers[id].localDescription,
+            returnAddress: receivedData.recipient,
+          });
+        }
+      };
+
+      peers[id].oniceconnectionstatechange = function(event) {
+        if (peers[id].iceConnectionState === 'completed') {
+          socket.emit('ice-merge', {
+            recipient: receivedData.returnAddress,
+            sdp: peers[id].localDescription,
+            returnAddress: receivedData.recipient,
+          });
+        } else if (peers[id].iceConnectionState === 'disconnected') {
+          removeVideo(receivedData.returnAddress);
+        }
+      };
+
+      peers[id].setRemoteDescription(new RTCSessionDescription(receivedData.offer))
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      peers[id].createAnswer(function (answerObj) {
+        peers[id].setLocalDescription(answerObj);
+        socket.emit('answer', {
+          answer: answerObj,
+          recipient: receivedData.returnAddress,
+          returnAddress: receivedData.recipient
+        });
+
+        console.log('Sent answer!');
+      }, function (error) {
+        console.log(error);
+      });
+    }, function (error) {
+      console.log(error);
+    });
+  };
+
+  socket.on('joined', function (data) {
+    console.log(data.message);
+    data.userIds.forEach(function (user, index) {
+      sendOffer(user, data.yourId);
+    });
+
+  }, function (error) {
+    console.log(error);
+  });
+
+  socket.on('created', function (data) {
+    console.log(data);
+  });
+
+  socket.on('offer', function (data) {
+    console.log('Got offer!'); 
+    sendAnswer(data);
+  });
+
+  socket.on('answer', function (data) {
+    console.log('Got answer!\n');
+    peers[data.returnAddress].setRemoteDescription(new RTCSessionDescription(data.answer))
+    .catch(function (error) {
+      console.log(error);
+    });
+  });
+
+  socket.on('ice-candidate', function (data) {
+    if (!!peers[data.returnAddress]) {
+      peers[data.returnAddress].addIceCandidate(new RTCIceCandidate(data.candidate))
+      .catch(function (error) {
+        console.error(error);
+      });
+    }
+  });
+
+  socket.on('ice-merge', function (data) {
+    console.log(peers);
+    peers[data.returnAddress].setRemoteDescription(new RTCSessionDescription(data.sdp))
+    .catch(function (error) {
+      console.log(error);
+    });
+  });
+
+
+  askForCamera();
+
+  socket.emit('check', {
+    roomName: room,
+    user: user
+  });
+
+};
+
+
+
+
+
+
+
--- mysqlSchema.sql
+++ mysqlSchema.sql
@@ -2,8 +2,8 @@
 -- Globals
 -- ---
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET FOREIGN_KEY_CHECKS=0;
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
 
 -- ---
 -- Table 'users'
@@ -15,40 +15,54 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
   `username` VARCHAR(255) NULL DEFAULT NULL,
-  `firstname` VARCHAR(255) NULL DEFAULT NULL,
-  `lastname` VARCHAR(255) NULL DEFAULT NULL,
+  `firstName` VARCHAR(255) NULL DEFAULT NULL,
+  `lastName` VARCHAR(255) NULL DEFAULT NULL,
   `email` VARCHAR(255) NULL DEFAULT NULL,
+  `avatarUrl` VARCHAR(255) NULL DEFAULT NULL,
+  `mobileNumber` VARCHAR(255) NOT NULL,
+  `avatarThumbUrl` VARCHAR(255) NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  `googleId` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY (`username`)
 );
 
 -- ---
--- Table 'classes'
+-- Table 'streams'
 -- 
 -- ---
 
-DROP TABLE IF EXISTS `classes`;
+DROP TABLE IF EXISTS `streams`;
     
-CREATE TABLE `classes` (
+CREATE TABLE `streams` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `classname` VARCHAR(255) NULL DEFAULT NULL,
-  `access` VARCHAR(255) NULL DEFAULT NULL,
-  `keywords` VARCHAR(255) NULL DEFAULT NULL,
-  `instructorid` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `title` VARCHAR(255) NULL DEFAULT NULL,
+  `online` VARCHAR(255) NOT NULL,
+  `creatorId` INTEGER NOT NULL,
+  `description` VARCHAR(255) NULL DEFAULT NULL,
+  `subscriberCount` INTEGER NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`title`)
 );
 
 -- ---
--- Table 'users_classes'
+-- Table 'subscriptions'
 -- 
 -- ---
 
-DROP TABLE IF EXISTS `users_classes`;
+DROP TABLE IF EXISTS `subscriptions`;
     
-CREATE TABLE `users_classes` (
+CREATE TABLE `subscriptions` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `classid` INTEGER NULL DEFAULT NULL,
-  `userid` INTEGER NULL DEFAULT NULL,
+  `streamId` INTEGER NOT NULL,
+  `userId` INTEGER NOT NULL,
+  `phoneNotifications` VARCHAR(255) NOT NULL,
+  `emailNotifications` VARCHAR(255) NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
 );
 
@@ -61,40 +75,122 @@ DROP TABLE IF EXISTS `schedules`;
     
 CREATE TABLE `schedules` (
   `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `dayofweek` INTEGER NULL DEFAULT NULL,
-  `starttime` TIME NULL DEFAULT NULL,
-  `endtime` TIME NULL DEFAULT NULL,
-  `classid` INTEGER NULL DEFAULT NULL,
+  `dayOfWeek` INTEGER NULL DEFAULT NULL,
+  `startTime` TIME NULL DEFAULT NULL,
+  `endTime` TIME NULL DEFAULT NULL,
+  `streamId` INTEGER NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
   PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'streams_keywords'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `streams_keywords`;
+    
+CREATE TABLE `streams_keywords` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `streamId` INTEGER NOT NULL,
+  `keywordId` INTEGER NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'keywords'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `keywords`;
+    
+CREATE TABLE `keywords` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` VARCHAR(255) NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`text`)
+);
+
+-- ---
+-- Table 'streams_categories'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `streams_categories`;
+    
+CREATE TABLE `streams_categories` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `streamId` INTEGER NULL DEFAULT NULL,
+  `categoryId` INTEGER NULL DEFAULT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'categories'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `categories`;
+    
+CREATE TABLE `categories` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `text` VARCHAR(255) NOT NULL,
+  `createdAt` DATETIME NOT NULL,
+  `updatedAt` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`text`)
 );
 
 -- ---
 -- Foreign Keys 
 -- ---
 
-ALTER TABLE `classes` ADD FOREIGN KEY (instructorid) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `users_classes` ADD FOREIGN KEY (classid) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `users_classes` ADD FOREIGN KEY (userid) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE `schedules` ADD FOREIGN KEY (classid) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `streams` ADD FOREIGN KEY (creatorId) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `subscriptions` ADD FOREIGN KEY (streamId) REFERENCES `streams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `subscriptions` ADD FOREIGN KEY (userId) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `schedules` ADD FOREIGN KEY (streamId) REFERENCES `streams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `streams_keywords` ADD FOREIGN KEY (streamId) REFERENCES `streams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `streams_keywords` ADD FOREIGN KEY (keywordId) REFERENCES `keywords` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `streams_categories` ADD FOREIGN KEY (id) REFERENCES `streams` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `streams_categories` ADD FOREIGN KEY (categoryId) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- ---
 -- Table Properties
 -- ---
 
 -- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `classes` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `users_classes` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `streams` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `subscriptions` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 -- ALTER TABLE `schedules` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `streams_keywords` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `keywords` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `streams_categories` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `categories` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `users` (`id`,`username`,`firstname`,`lastname`,`email`) VALUES
+-- INSERT INTO `users` (`id`,`username`,`firstName`,`lastName`,`email`,`avatarUrl`,`mobileNumber`,`avatarThumbUrl`,`createdAt`,`updatedAt`,`googleId`) VALUES
+-- ('','','','','','','','','','','');
+-- INSERT INTO `streams` (`id`,`title`,`online`,`creatorId`,`description`,`subscriberCount`,`createdAt`,`updatedAt`) VALUES
+-- ('','','','','','','','');
+-- INSERT INTO `subscriptions` (`id`,`streamId`,`userId`,`phoneNotifications`,`emailNotifications`,`createdAt`,`updatedAt`) VALUES
+-- ('','','','','','','');
+-- INSERT INTO `schedules` (`id`,`dayOfWeek`,`startTime`,`endTime`,`streamId`,`createdAt`,`updatedAt`) VALUES
+-- ('','','','','','','');
+-- INSERT INTO `streams_keywords` (`id`,`streamId`,`keywordId`,`createdAt`,`updatedAt`) VALUES
 -- ('','','','','');
--- INSERT INTO `classes` (`id`,`classname`,`access`,`keywords`,`instructorid`) VALUES
+-- INSERT INTO `keywords` (`id`,`text`,`createdAt`,`updatedAt`) VALUES
+-- ('','','','');
+-- INSERT INTO `streams_categories` (`id`,`streamId`,`categoryId`,`createdAt`,`updatedAt`) VALUES
 -- ('','','','','');
--- INSERT INTO `users_classes` (`id`,`classid`,`userid`) VALUES
--- ('','','');
--- INSERT INTO `schedules` (`id`,`dayofweek`,`starttime`,`endtime`,`classid`) VALUES
--- ('','','','','');
+-- INSERT INTO `categories` (`id`,`text`,`createdAt`,`updatedAt`) VALUES
+-- ('','','','');
