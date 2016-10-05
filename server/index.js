bb5ebafd70163ed3949895888d13e945c6cbad77
ZolayvarE <ZolayvarECU@gmail.com>
2016-10-04 20:07:49 -0700

    Make http requests reroute to https serier

--- server/index.js
+++ server/index.js
@@ -4,3 +4,20 @@ var port = process.env.PORT || 8443;
 
 server.listen(port);
 console.log('Listening on port: ' + port);
+
+
+
+var app = require('express')();
+
+app.get('*', function (req, res) {
+	res.redirect('https://localhost:8443' + req.url);
+});
+
+app.listen(8080);
+
+
+
+
+
+
+
