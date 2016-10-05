c4cbdbe425c70380c80e3a0f451ff79ee9431479
cassac <hicassac@gmail.com>
2016-10-05 11:58:52 -0700

    implements authentication for private routes

---
+++ client/components/RequireAuth.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import { connect } from 'react-redux';
+
+
+const RequireAuth = (WrappedComponent) => {
+  class Auth extends React.Component {
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        browserHistory.push('/login');
+      }
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.auth.authenticated };
+  }
+
+  return connect(mapStateToProps)(Auth);
+}
+
+export default RequireAuth;
