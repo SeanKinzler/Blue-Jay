4e8a604ea7fa756c4ec7bd81122df42c2458bae8
ZolayvarE <ZolayvarECU@gmail.com>
2016-10-06 18:20:43 -0700

    Update timeout for video failure

--- client/lib/webRTC.js
+++ client/lib/webRTC.js
@@ -51,191 +51,192 @@ module.exports = function (room, user, socket) {
     audio: true,
   }, function (stream) {
     parentStream = stream;
-  }, function (error) {
-    console.log(error);
-  });
 
-  var askForCamera = function () {
-    navigator.getUserMedia({
-      video: true,
-      audio: false, 
-    }, function (stream) {
-      localSrc = window.URL.createObjectURL(stream);
+    var askForCamera = function () {
+      navigator.getUserMedia({
+        video: true,
+        audio: false, 
+      }, function (stream) {
+        localSrc = window.URL.createObjectURL(stream);
 
-      var localVideoPort = document.getElementById('localVideo');
-      if (localVideoPort) {
-        localVideoPort.src = localSrc;
-      } else {
-        document.getElementById('remoteVideo').src = localSrc;
-      }
-    }, function (error) {
-      setTimeout(function () {
-        askForCamera();
-      }, 500);
-    });
-  };
-
-  askForCamera();
-
-  var sendOffer = function (targetUserId, yourUserId) {
-    parent = new RTCPeerConnection(ICE);
-
-    peers[targetUserId] = parent;
-
-    parent.addStream(parentStream);
-
-    parent.onaddstream = function (media) {
-      parentStream = media.stream;
-      addVideo(media.stream, targetUserId);
+        var localVideoPort = document.getElementById('localVideo');
+        if (localVideoPort) {
+          localVideoPort.src = localSrc;
+        } else {
+          document.getElementById('remoteVideo').src = localSrc;
+        }
+      }, function (error) {
+        setTimeout(function () {
+          askForCamera();
+        }, 500);
+      });
     };
 
-    parent.onicecandidate = function (event) {
-      if (!!event.candidate) {
-        socket.emit('ice-candidate', {
+    askForCamera();
+
+    var sendOffer = function (targetUserId, yourUserId) {
+      parent = new RTCPeerConnection(ICE);
+
+      peers[targetUserId] = parent;
+
+      parent.addStream(parentStream);
+
+      parent.onaddstream = function (media) {
+        parentStream = media.stream;
+        addVideo(media.stream, targetUserId);
+      };
+
+      parent.onicecandidate = function (event) {
+        if (!!event.candidate) {
+          socket.emit('ice-candidate', {
+            recipient: targetUserId,
+            candidate: event.candidate,
+            returnAddress: yourUserId,
+          });
+        }
+      };
+
+      parent.oniceconnectionstatechange = function(event) {
+        if (parent.iceConnectionState === 'disconnected') {
+          peers[targetUserId].close();
+          delete peers[targetUserId];
+          if (!host) {
+            removeVideo(targetUserId);
+          }
+        } else if (parent.iceConnectionState === 'completed') {
+          var localStream = peers[targetUserId].getLocalStreams()[0];
+          localStream.removeTrack(localStream.getAudioTracks()[0]);
+        }
+      };
+
+      parent.createOffer(function (offerObj) {
+        parent.setLocalDescription(offerObj);
+        socket.emit('offer', {
           recipient: targetUserId,
-          candidate: event.candidate,
+          offer: offerObj,
           returnAddress: yourUserId,
         });
-      }
-    };
-
-    parent.oniceconnectionstatechange = function(event) {
-      if (parent.iceConnectionState === 'disconnected') {
-        peers[targetUserId].close();
-        delete peers[targetUserId];
-        if (!host) {
-          removeVideo(targetUserId);
-        }
-      } else if (parent.iceConnectionState === 'completed') {
-        var localStream = peers[targetUserId].getLocalStreams()[0];
-        localStream.removeTrack(localStream.getAudioTracks()[0]);
-      }
-    };
-
-    parent.createOffer(function (offerObj) {
-      parent.setLocalDescription(offerObj);
-      socket.emit('offer', {
-        recipient: targetUserId,
-        offer: offerObj,
-        returnAddress: yourUserId,
+      }, function (error) {
+        console.log(error);
       });
+    };
+
+    var sendAnswer = function (receivedData) {
+      peers.length++;
+      var id = receivedData.returnAddress;
+
+      peers[id] = new RTCPeerConnection(ICE);
+
+      peers[id].addStream(parentStream);
+
+      peers[id].onicecandidate = function (event) {
+        if (!!event.candidate) {
+          socket.emit('ice-candidate', {
+            recipient: receivedData.returnAddress,
+            candidate: event.candidate,
+            returnAddress: receivedData.recipient,
+          });
+        }
+      };
+
+      peers[id].oniceconnectionstatechange = function(event) {
+        if (peers[id].iceConnectionState === 'disconnected') {
+          if (peers[id]) { 
+            peers[id].close(); 
+            delete peers[id];
+          }
+
+          if (!host) {
+            removeVideo(receivedData.returnAddress);
+          }
+        }
+      };
+
+      peers[id].setRemoteDescription(new RTCSessionDescription(receivedData.offer))
+      .catch(function (error) {
+        console.log(error);
+      });
+
+      peers[id].createAnswer(function (answerObj) {
+        peers[id].setLocalDescription(answerObj);
+        socket.emit('answer', {
+          answer: answerObj,
+          recipient: receivedData.returnAddress,
+          returnAddress: receivedData.recipient
+        });
+
+      }, function (error) {
+        console.log(error);
+      });
+    };
+
+
+    socket.on('RTC-target', function (data) {
+      host = false;
+      if (peers[data.deleteTarget]) {
+        peers[data.deleteTarget].close();
+        delete peers[data.deleteTarget];
+      }
+
+      data.userIds.forEach(function (user, index) {
+        sendOffer(user, data.yourId);
+      });
+
     }, function (error) {
       console.log(error);
     });
-  };
 
-  var sendAnswer = function (receivedData) {
-    peers.length++;
-    var id = receivedData.returnAddress;
+    socket.on('created', function (data) {
+      host = true;
+      document.getElementById('remoteVideo').src = localSrc;
 
-    peers[id] = new RTCPeerConnection(ICE);
+      var ownVideo = document.getElementById('localVideo');
+      ownVideo.parentNode.removeChild(ownVideo);
+    });
 
-    peers[id].addStream(parentStream);
+    socket.on('offer', function (data) {
+      sendAnswer(data);
+    });
 
-    peers[id].onicecandidate = function (event) {
-      if (!!event.candidate) {
-        socket.emit('ice-candidate', {
-          recipient: receivedData.returnAddress,
-          candidate: event.candidate,
-          returnAddress: receivedData.recipient,
+    socket.on('answer', function (data) {
+      peers[data.returnAddress].setRemoteDescription(new RTCSessionDescription(data.answer))
+      .then(function () {
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    });
+
+    socket.on('ice-candidate', function (data) {
+      if (!!peers[data.returnAddress]) {
+        peers[data.returnAddress].addIceCandidate(new RTCIceCandidate(data.candidate))
+        .catch(function (error) {
+          console.error(error);
         });
       }
-    };
-
-    peers[id].oniceconnectionstatechange = function(event) {
-      if (peers[id].iceConnectionState === 'disconnected') {
-        if (peers[id]) { 
-          peers[id].close(); 
-          delete peers[id];
-        }
-
-        if (!host) {
-          removeVideo(receivedData.returnAddress);
-        }
-      }
-    };
-
-    peers[id].setRemoteDescription(new RTCSessionDescription(receivedData.offer))
-    .catch(function (error) {
-      console.log(error);
     });
 
-    peers[id].createAnswer(function (answerObj) {
-      peers[id].setLocalDescription(answerObj);
-      socket.emit('answer', {
-        answer: answerObj,
-        recipient: receivedData.returnAddress,
-        returnAddress: receivedData.recipient
+    socket.on('start', function (data) {
+      socket.emit('stop', {
+        roomName: room,
+        user: user,
+        time: data,
       });
-
-    }, function (error) {
-      console.log(error);
     });
-  };
 
+    window.checkForHelp = setInterval(function () {
+      if (!host && document.getElementById('remoteVideo').src.slice(0, 4) !== 'blob') {
+        parent = null;
+        socket.emit('ready');
+      }
+    }, 1000);
 
-  socket.on('RTC-target', function (data) {
-    host = false;
-    if (peers[data.deleteTarget]) {
-      peers[data.deleteTarget].close();
-      delete peers[data.deleteTarget];
-    }
-
-    data.userIds.forEach(function (user, index) {
-      sendOffer(user, data.yourId);
-    });
+    socket.emit('ready');
 
   }, function (error) {
     console.log(error);
   });
-
-  socket.on('created', function (data) {
-    host = true;
-    document.getElementById('remoteVideo').src = localSrc;
-
-    var ownVideo = document.getElementById('localVideo');
-    ownVideo.parentNode.removeChild(ownVideo);
-  });
-
-  socket.on('offer', function (data) {
-    sendAnswer(data);
-  });
-
-  socket.on('answer', function (data) {
-    peers[data.returnAddress].setRemoteDescription(new RTCSessionDescription(data.answer))
-    .then(function () {
-
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  });
-
-  socket.on('ice-candidate', function (data) {
-    if (!!peers[data.returnAddress]) {
-      peers[data.returnAddress].addIceCandidate(new RTCIceCandidate(data.candidate))
-      .catch(function (error) {
-        console.error(error);
-      });
-    }
-  });
-
-  socket.on('start', function (data) {
-    socket.emit('stop', {
-      roomName: room,
-      user: user,
-      time: data,
-    });
-  });
-
-  window.checkForHelp = setInterval(function () {
-    if (!host && document.getElementById('remoteVideo').src.slice(0, 4) !== 'blob') {
-      parent = null;
-      socket.emit('ready');
-    }
-  }, 2000);
-
-  socket.emit('ready');
 };
 
 
