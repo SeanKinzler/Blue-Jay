af83ad59562c3d4cb02151160a21beb6b4a4d225
ZolayvarE <ZolayvarECU@gmail.com>
2016-10-07 21:46:22 -0700

    Fix linting error

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -5,43 +5,43 @@ import * as Actions from '../actions/index.jsx';
 
 class Login extends Component {
 
-	signInHandler(form) {
-		this.props.signInUser({
-			username: form.username.value,
-			password: form.password.value
-		});
-	}
+  signInHandler(form) {
+    this.props.signInUser({
+      username: form.username.value,
+      password: form.password.value
+    });
+  }
 
-	render() {
-		return (
-			<div className='container'>
-				<form 
-					onSubmit={ (e) => { e.preventDefault(); this.signInHandler(e.target); } } 
-					className='col s6 offset-s6'>
-					<div className='row'>
-						<div className='input-field col s6 offset-s3' >
-							<input id='username' type='text' name='username' />
-							<label htmlFor="username">Username</label>
-						</div>
-					</div>
-					<div className='row'>
-						<div className='input-field col s6 offset-s3' >
-							<input id='password' type='password' name='password'/>
-							<label htmlFor="password">Password</label>
-						</div>
-					</div>
-					<div className='row'>
-						<div className='input-field col s6 offset-s3' >
-							<input className='btn' type='submit' value='Login' />
-						</div>
-					</div>
-				</form>
-				<div className='center-align'>
-					<Link to='signup'>Signup</Link> for an account.
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className='container'>
+        <form 
+          onSubmit={ (e) => { e.preventDefault(); this.signInHandler(e.target); } } 
+          className='col s6 offset-s6'>
+          <div className='row'>
+            <div className='input-field col s6 offset-s3' >
+              <input id='username' type='text' name='username' />
+              <label htmlFor="username">Username</label>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='input-field col s6 offset-s3' >
+              <input id='password' type='password' name='password'/>
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='input-field col s6 offset-s3' >
+              <input className='btn' type='submit' value='Login' />
+            </div>
+          </div>
+        </form>
+        <div className='center-align'>
+          <Link to='signup'>Signup</Link> for an account.
+        </div>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
