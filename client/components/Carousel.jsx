c4519c84f6939ce54fcdbf2b7aa0b3c96420e10a
ZolayvarE <ZolayvarECU@gmail.com>
2016-10-07 21:42:04 -0700

    Fix linting error

--- client/components/Carousel.jsx
+++ client/components/Carousel.jsx
@@ -2,52 +2,52 @@ import React, { Component } from 'react';
 import {Link} from 'react-router';
 
 export default class Carousel extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			timeoutId: ''
-		};
-	}
-	
-	carouselSlide() {
-		return setInterval(function(){
-		  $('.carousel').carousel('next');
-		}, 3000);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeoutId: ''
+    };
+  }
+  
+  carouselSlide() {
+    return setInterval(function() {
+      $('.carousel').carousel('next');
+    }, 3000);
+  }
 
-	componentDidMount() {
-		var timeoutId = this.carouselSlide();
-		this.setState({timeoutId: timeoutId});
-		$('.carousel.carousel-slider').carousel({full_width: true});
-	}
+  componentDidMount() {
+    var timeoutId = this.carouselSlide();
+    this.setState({timeoutId: timeoutId});
+    $('.carousel.carousel-slider').carousel({full_width: true});
+  }
 
-	componentWillUnmount() {
-		clearInterval(this.state.timeoutId);
-	}
+  componentWillUnmount() {
+    clearInterval(this.state.timeoutId);
+  }
 
-	render() {
-		return (
-			<div className="carousel carousel-slider center" data-indicators="true">
-			  <div className="carousel-fixed-item center">
-			  	<Link 
-			  		to='login' 
-			  		className='btn waves-effect white grey-text darken-text-2'>
-			  		Login/Signup
-			  	</Link>
-			  </div>
-			  <div className="carousel-item red white-text" href="#one!">
-			    <h2>Education</h2>
-			    <p className="white-text">Attend or teach a class</p>
-			  </div>
-			  <div className="carousel-item amber white-text" href="#two!">
-			    <h2>Entertainment</h2>
-			    <p className="white-text">Stream yourself to friends</p>
-			  </div>
-			  <div className="carousel-item green white-text" href="#three!">
-			    <h2>Everyone</h2>
-			    <p className="white-text">Do you stream?</p>
-			  </div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="carousel carousel-slider center" data-indicators="true">
+        <div className="carousel-fixed-item center">
+          <Link 
+            to='login' 
+            className='btn waves-effect white grey-text darken-text-2'>
+            Login/Signup
+          </Link>
+        </div>
+        <div className="carousel-item red white-text" href="#one!">
+          <h2>Education</h2>
+          <p className="white-text">Attend or teach a class</p>
+        </div>
+        <div className="carousel-item amber white-text" href="#two!">
+          <h2>Entertainment</h2>
+          <p className="white-text">Stream yourself to friends</p>
+        </div>
+        <div className="carousel-item green white-text" href="#three!">
+          <h2>Everyone</h2>
+          <p className="white-text">Do you stream?</p>
+        </div>
+      </div>
+    )
+  }
 }
--- client/pages/Search.jsx
+++ client/pages/Search.jsx
@@ -9,136 +9,136 @@ import SearchResultsModal from '../components/SearchResultsModal.jsx';
 
 class Search extends Component {
 
-	makeStreamQuery() {
-		this.props.getStreams();
-	}
+  makeStreamQuery() {
+    this.props.getStreams();
+  }
 
-	componentDidMount() {
-		$('select').material_select();
-		$('select').on(
-			'change', 
-			(e) => {
-				var optionsText = [];
-				var title = e.target.id;
-				var handler = e.target.dataset.handler;
-				var optionsElements = e.target.selectedOptions;
-				for (var i = 0; i < optionsElements.length; i++) {
-					optionsText.push(optionsElements[i].text);
-				}
-				this.props[handler](optionsText)
-			}
-		) 
-	}
+  componentDidMount() {
+    $('select').material_select();
+    $('select').on(
+      'change', 
+      (e) => {
+        var optionsText = [];
+        var title = e.target.id;
+        var handler = e.target.dataset.handler;
+        var optionsElements = e.target.selectedOptions;
+        for (var i = 0; i < optionsElements.length; i++) {
+          optionsText.push(optionsElements[i].text);
+        }
+        this.props[handler](optionsText);
+      }
+    ); 
+  }
 
-	changeViewHandler(view) {
-		this.props.toggleSearchResultsView(view)
-	}
+  changeViewHandler(view) {
+    this.props.toggleSearchResultsView(view);
+  }
 
-	renderFilterView() {
-		let compact = this.props.streams.view === 'compact' ? 'blue' : '';
-		let extended = this.props.streams.view === 'extended' ? 'blue' : '';
-		return (
-			<span className='align-center'>
-				<i onClick={ () => {this.changeViewHandler('compact')} } className={`material-icons ${compact}`}>view_module</i>
-				<i onClick={ () => {this.changeViewHandler('extended')} } className={`material-icons ${extended}`}>view_list</i>
-			</span>
-		)
-	}
+  renderFilterView() {
+    let compact = this.props.streams.view === 'compact' ? 'blue' : '';
+    let extended = this.props.streams.view === 'extended' ? 'blue' : '';
+    return (
+      <span className='align-center'>
+        <i onClick={ () => { this.changeViewHandler('compact'); } } className={`material-icons ${compact}`}>view_module</i>
+        <i onClick={ () => { this.changeViewHandler('extended'); } } className={`material-icons ${extended}`}>view_list</i>
+      </span>
+    );
+  }
 
-	renderSearchResults() {
-		let searchResults = this.props.streams.data;
-		if (this.props.streams.view === 'compact') {
-			return (
-				<SearchResultsCompact 
-					searchResults={searchResults} 
-					openModal={this.props.openModal}
-				/>
-			)
-		}
-		else if (this.props.streams.view === 'extended') {
-			return (
-				<SearchResultsExtended 
-					searchResults={searchResults}
-					openModal={this.props.openModal}
-				/>
-			)
-		}
-	}
+  renderSearchResults() {
+    let searchResults = this.props.streams.data;
+    if (this.props.streams.view === 'compact') {
+      return (
+        <SearchResultsCompact 
+          searchResults={searchResults} 
+          openModal={this.props.openModal}
+        />
+      )
+    }
+    else if (this.props.streams.view === 'extended') {
+      return (
+        <SearchResultsExtended 
+          searchResults={searchResults}
+          openModal={this.props.openModal}
+        />
+      )
+    }
+  }
 
-	render() {
-		return (
-			<div className='container'>
-				<SearchBar onTermChange={this.props.searchChannels} />
-				<SearchResultsModal 
-					selectedStream={this.props.modal.selectedStream} 
-					modalIsOpen={this.props.modal.modalIsOpen}
-					onRequestClose={ () => { this.props.closeModal() } }
-				/>
-				<div className='row'>
-					<div className='col s4 m2'>
-						<SearchFilter filterOptions={this.props.categories} />
-					</div>
-					<div className='col s4 m2'>
-						<SearchFilter filterOptions={this.props.prices} />
-					</div>
-					<div className='col s4 m2'>
-						<SearchFilter filterOptions={this.props.types} />
-					</div>
-					<div className='col s4 m2'>
-						<SearchFilter filterOptions={this.props.days} />
-					</div>
-					<div className='col s4 m2'>
-						<SearchFilter filterOptions={this.props.times} />
-					</div>
-					<div className='col s4 m2'>
-						{ this.renderFilterView() }
-					</div>
-				</div>
-				<div className='row'>
-					{ this.renderSearchResults() }
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className='container'>
+        <SearchBar onTermChange={this.props.searchChannels} />
+        <SearchResultsModal 
+          selectedStream={this.props.modal.selectedStream} 
+          modalIsOpen={this.props.modal.modalIsOpen}
+          onRequestClose={ () => { this.props.closeModal() } }
+        />
+        <div className='row'>
+          <div className='col s4 m2'>
+            <SearchFilter filterOptions={this.props.categories} />
+          </div>
+          <div className='col s4 m2'>
+            <SearchFilter filterOptions={this.props.prices} />
+          </div>
+          <div className='col s4 m2'>
+            <SearchFilter filterOptions={this.props.types} />
+          </div>
+          <div className='col s4 m2'>
+            <SearchFilter filterOptions={this.props.days} />
+          </div>
+          <div className='col s4 m2'>
+            <SearchFilter filterOptions={this.props.times} />
+          </div>
+          <div className='col s4 m2'>
+            { this.renderFilterView() }
+          </div>
+        </div>
+        <div className='row'>
+          { this.renderSearchResults() }
+        </div>
+      </div>
+    )
+  }
 } 
 
 const mapStateToProps = (state) => {
-	return {
-		modal: state.modal,
-		streams: state.streams,
-		categories: {
-			title: 'Categories', 
-			handler: 'filterStreamCategories',
-			options: ['Math', 'Entertainment', 'History', 'Politics'],
-			selected: state.streams.categories
-		},
-		prices: {
-			title: 'Price',
-			handler: 'filterStreamPrices',
-			options: ['Free', 'Paid'],
-			selected: state.streams.prices
-		},
-		types: {
-			title: 'Type',
-			handler: 'filterStreamTypes',
-			options: ['Live', 'Archived'],
-			selected: state.streams.types
-		},
-		days: {
-			title: 'Day',
-			handler: 'filterStreamDays',
-			options: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-			selected: state.streams.days
-		},
-		times: {
-			title: 'Time',
-			handler: 'filterStreamTimes',
-			options: ['12:00am', '1:00am', '2:00am', '3:00am', '4:00am', '5:00am', '6:00am', '7:00am', 
-					'8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm',
-					'4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm', '10:00pm', '11:00pm'],
-			selected: state.streams.times
-		}		
-	}
+  return {
+    modal: state.modal,
+    streams: state.streams,
+    categories: {
+      title: 'Categories', 
+      handler: 'filterStreamCategories',
+      options: ['Math', 'Entertainment', 'History', 'Politics'],
+      selected: state.streams.categories
+    },
+    prices: {
+      title: 'Price',
+      handler: 'filterStreamPrices',
+      options: ['Free', 'Paid'],
+      selected: state.streams.prices
+    },
+    types: {
+      title: 'Type',
+      handler: 'filterStreamTypes',
+      options: ['Live', 'Archived'],
+      selected: state.streams.types
+    },
+    days: {
+      title: 'Day',
+      handler: 'filterStreamDays',
+      options: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      selected: state.streams.days
+    },
+    times: {
+      title: 'Time',
+      handler: 'filterStreamTimes',
+      options: ['12:00am', '1:00am', '2:00am', '3:00am', '4:00am', '5:00am', '6:00am', '7:00am', 
+          '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm',
+          '4:00pm', '5:00pm', '6:00pm', '7:00pm', '8:00pm', '9:00pm', '10:00pm', '11:00pm'],
+      selected: state.streams.times
+    }   
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
