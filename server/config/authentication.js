162c89288395b08c6aa5a628f976e41a7f7ecbf7
ZolayvarE <ZolayvarECU@gmail.com>
2016-10-10 10:27:49 -0700

    Update auth middleware

--- server/config/authentication.js
+++ server/config/authentication.js
@@ -26,7 +26,7 @@ var checkToken = function (req, res, next) {
         res.sendStatus(302);
 
       } else {
-        
+
         res.sendStatus(200);
 
       }
@@ -54,7 +54,7 @@ var authMiddleware = function (req, res, next) {
       } else {
 
         req.username = decoded.username;
-        req.id = decoded.id;
+        req.userId = decoded.id;
 
         next();
 
