c213f155c8f706604981ae48bcd8eccc0ab99e55
cassac <hicassac@gmail.com>
2016-10-11 12:21:20 -0700

    adds stream details

--- client/components/UserSubscriptions.jsx
+++ client/components/UserSubscriptions.jsx
@@ -18,13 +18,13 @@ const UserSubscriptions = ({subscriptions, removeSubscription}) => {
 								<div className="card-content white-text">
 									<span className="card-title">{ stream.title }</span>
 									<p>{ stream.description }</p>
-									<span>*Stream titles don't work well as urls. Use slugs.</span>
+									<span>{ stream.creatorName }</span>
 									<Link to='stream/one' className="secondary-content"><i className="material-icons">contact_phone</i></Link>
 								</div>
 								<div className="card-action">
 									<span>{ stream.creatorId }</span>
 									{ isOnline(stream) }
-									<span className="badge green">{ stream.subscriberCount }</span>
+									<span className="badge">{ stream.subscriberCount }</span>
 									<span onClick={ () => { removeSubscription(stream) } }>Remove</span>
 								</div>
 							</div>
