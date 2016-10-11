ff6cd4502dc4c33ac99b50bebdb445f127adc34d
cassac <hicassac@gmail.com>
2016-10-11 11:14:18 -0700

    redesigns layout

--- client/components/SearchResultsExtended.jsx
+++ client/components/SearchResultsExtended.jsx
@@ -1,45 +1,41 @@
 import React from 'react';
 
-const SearchResultsExtended = ({searchResults, openModal, addSubscription}) => {
+const SearchResultsExtended = ({searchResults, openModal, addSubscription, subscriptions}) => {
+	const isSubscribed = (stream) => {
+
+	}
+
 	const isOnline = (stream) => {
-		if (stream.online) {
-			return <span className="badge red">{ stream.online }</span>
+		if (stream.online === 'true') {
+			return <i className="material-icons">volume_up</i>;
 		} else {
-			return <span></span>;
+			return <i className="material-icons">volume_off</i>;
 		}
 	}
 	return (
 		<div>
 			{ searchResults.map((stream) => {
 				return (
-					<div key={stream.id} className='col s12'>
-							<div className="card blue">
-								<div className="card-content white-text">
-									<span className="card-title">{ stream.title }</span>
-									<p>{ stream.description }</p>
-								</div>
-								<div className="card-action">
-									<div className='row'>
-										<div className='col s2'>
-											<i onClick={ () => { addSubscription(stream) } } className='material-icons circle green'>done</i>
-										</div>
-										<div className='col s2'>
-											<div onClick={ () => { openModal(stream); } } className='btn'>Details</div>
-										</div>
-										<div className='col s2'>
-											<span className='white-text'>{ stream.creatorId }</span>
-										</div>
-										<div className='col s2'>
-											<span className="badge red">{ stream.online }</span>
-										</div>
-										<div className='col s2'>
-											<span className="badge white-text">{ stream.subscriberCount }</span>
-										</div>
+				<ul key={stream.title} className="collection with-header col s12">
+				  <li className="collection-header">
+				  	<h5>{stream.title}</h5>
+				  	<p>{stream.description}</p>
+				  </li>
+				  <li className="collection-item">
+				  	<table className='centered'>
+				  		<tbody>
+				  			<tr>
+									<td><i onClick={ () => { addSubscription(stream) } } className='material-icons circle green'>done</i></td>
+									<td><a onClick={ () => { openModal(stream); } } className='btn'>Details</a></td>
+									<td>Creator:<br/>{ stream.creatorId }</td>
+									<td>{ isOnline(stream) }</td>
+									<td><i className="material-icons">supervisor_account</i><br/>{ stream.subscriberCount }</td>
+				  			</tr>
+				  		</tbody>
+				  	</table>
 
-									</div>
-								</div>
-							</div>
-					</div>
+				  </li>
+				</ul>
 				)
 			})}
 		</div>
