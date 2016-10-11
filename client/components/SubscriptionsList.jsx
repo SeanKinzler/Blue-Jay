9eb61f5a11e476e6ad298397051890309b705715
ZolayvarE <ZolayvarECU@gmail.com>
2016-10-11 14:14:17 -0700

    Fix multiple bugs in UI routing

--- client/components/SubscriptionsList.jsx
+++ client/components/SubscriptionsList.jsx
@@ -5,34 +5,46 @@ import allColors from '../lib/allColors.js';
 import urlHelper from '../utils/urlHelper.jsx';
 
 export default ({subscriptions}) => {
-	return (
-		<ul className="collection with-header">
-		  <li className='collection-header'>
-		  	<h5>Subscriptions</h5>
-		  </li>
+  subscriptions = subscriptions.filter(function (item) {
+    return !!item.title;
+  });
 
-		  { subscriptions.map((sub, i) => {
-		  	return (
-				  <li key={i} className="collection-item avatar">
-				    <i className={`material-icons circle ${ allColors() }`}>{ allIcons() }</i>
-				    <Link to={ `username/${ urlHelper.slugify(sub.title) }` } >
-					    <h3 className="title">{ sub.title }</h3>
-					  </Link>
-				    <p>{ sub.description }
-				    </p>
-				    <Link to={ `${ localStorage.username + '/' + urlHelper.slugify(sub.title) }` } className="secondary-content">
-				    	<i className="material-icons">contact_phone</i>
-				    </Link>
-				  </li>
-		  	)
-		  })}
+  if (subscriptions.length) {
+    return (
+      <ul className="collection with-header">
+        <li className='collection-header'>
+          <h5>Subscriptions</h5>
+        </li>
+
+        { subscriptions.map((sub, i) => {
+          return (
+            <li key={i} className="collection-item avatar">
+              <i className={`material-icons circle ${ allColors() }`}>{ allIcons() }</i>
+              <Link to={ `username/${ urlHelper.slugify(sub.title) }` } >
+                <h3 className="title">{ sub.title }</h3>
+              </Link>
+              <p>{ sub.description }
+              </p>
+              <Link to={ `${ localStorage.username + '/' + urlHelper.slugify(sub.title) }` } className="secondary-content">
+                <i className="material-icons">contact_phone</i>
+              </Link>
+            </li>
+          );
+        })}
 
 
-		  <li className="collection-header">
-			<Link to='subscriptions'>
-				<strong>All My Subscriptions</strong>
-			</Link>
-		  </li>		  
-		</ul>
-	)
-}
+        <li className="collection-header">
+        <Link to='subscriptions'>
+          <strong>All My Subscriptions</strong>
+        </Link>
+        </li>     
+      </ul>
+    );
+  } else {
+    return (
+      <div>
+      </div>
+    );
+  }
+
+};
