4c8a91a07b263bd5007cbe47f4aedd595912a9b5
cassac <hicassac@gmail.com>
2016-10-12 19:06:53 -0700

    connects stream room to redux store

--- client/pages/StreamRoom.jsx
+++ client/pages/StreamRoom.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as Actions from '../actions/index.jsx';
+import io from 'socket.io-client';
 import Video from '../components/Video.jsx';
 import InfoBox from '../components/InfoBox.jsx';
 import ChatContainer from '../components/ChatContainer.jsx';
 import Whiteboard from '../components/Whiteboard.jsx';
-import {Link} from 'react-router';
-import io from 'socket.io-client';
+import urlHelper from '../utils/urlHelper.jsx';
 
 const data = {
   title: 'cat',
@@ -14,21 +18,45 @@ const data = {
   category: 'Art'
 };
 
-export default class StreamRoom extends Component {
+class StreamRoom extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      socket: io.connect(),
-    };
+      socket: io.connect()
+    }
+  }
+
+  componentDidMount() {
+    const streamName = urlHelper.deslugify(this.props.params.streamSlug);
+    this.props.requestStreamInfo(streamName);
+  }
+
+  componentWillUnmount() {
+    this.props.leaveStream();
   }
 
   render() {
     return (
-      <div className='stream'>
+      <div className='container'>
         <Video socket={ this.state.socket } />
-        <InfoBox stuff={data}/>
+        <InfoBox video={this.props.video} creatorName={this.props.params.creatorName} />
         <Whiteboard socket={ this.state.socket } />
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    video: state.video.stream
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(Actions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, Actions)(StreamRoom);
