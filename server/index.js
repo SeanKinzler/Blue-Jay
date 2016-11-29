75095d0ab07450206dd2c292eea05d90ec2134c7
seankinzler <sean.kinzler92@gmail.com>
2016-11-29 15:06:04 -0800

    removed chadroulette reference

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ console.log('Listening on port: ' + port);
 var app = require('express')();
 
 app.get('*', function (req, res) {
-  res.redirect('https://www.chadroulette.com' + req.url);
+  res.redirect('https://www.bluejy.com' + req.url);
 });
 
 app.listen(8080);
